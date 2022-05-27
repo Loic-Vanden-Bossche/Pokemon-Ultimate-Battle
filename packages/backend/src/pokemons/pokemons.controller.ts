dd418b75983ab1149eed85af5d391d79e94710fc
Loïc Vanden Bossche <l.vandenbossche.jouvet@gmail.com>
2022-05-27 10:49:16 +0200

    Use of fs promises

--- packages/backend/src/pokemons/pokemons.controller.ts
+++ packages/backend/src/pokemons/pokemons.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Param, StreamableFile } from '@nestjs/common';
 import { PokemonsService } from './pokemons.service';
 import { ApiOperation } from '@nestjs/swagger';
-import { firstValueFrom, map } from "rxjs";
+import { firstValueFrom, map } from 'rxjs';
 
 @Controller('pokemons')
 export class PokemonsController {
@@ -28,12 +28,20 @@ export class PokemonsController {
   @Get('/:name/gif/front')
   @ApiOperation({ summary: 'Get front sprite for a specific pokemon' })
   async frontSprite(@Param('name') name: string) {
-    return firstValueFrom(this.pokemonsService.frontSprite(name).pipe(map(buffer => new StreamableFile(buffer))));
+    return this.pokemonsService
+      .backSprite(name)
+      .then((o) =>
+        firstValueFrom(o.pipe(map((buffer) => new StreamableFile(buffer)))),
+      );
   }
 
   @Get('/:name/gif/back')
   @ApiOperation({ summary: 'Get back sprite for a specific pokemon' })
   async backSprite(@Param('name') name: string) {
-    return firstValueFrom(this.pokemonsService.frontSprite(name).pipe(map(buffer => new StreamableFile(buffer))));
+    return this.pokemonsService
+      .frontSprite(name)
+      .then((o) =>
+        firstValueFrom(o.pipe(map((buffer) => new StreamableFile(buffer)))),
+      );
   }
 }
--- packages/backend/src/pokemons/pokemons.service.ts
+++ packages/backend/src/pokemons/pokemons.service.ts
@@ -1,10 +1,17 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 import { HttpService } from '@nestjs/axios';
-import * as fs from 'fs';
+import * as fsc from 'fs';
+import * as fs from 'fs/promises';
 import * as path from 'path';
 import { Pokemon } from './pokemon';
-import { map, Observable } from "rxjs";
+import { map, Observable } from 'rxjs';
+
+const fileExists = (p) =>
+  fs
+    .access(p, fsc.constants.F_OK)
+    .then(() => true)
+    .catch(() => false);
 
 @Injectable()
 export class PokemonsService {
@@ -12,50 +19,52 @@ export class PokemonsService {
 
   pokemonsPath = path.join(process.cwd(), 'pokemons-data');
 
-  findAll(): Pokemon[] {
-    if (!fs.existsSync(this.pokemonsPath)) {
+  findAll(): Promise<Pokemon[]> {
+    if (!fileExists(this.pokemonsPath)) {
       throw new HttpException('Pokemons not retrieved.', HttpStatus.NOT_FOUND);
     }
 
-    return this.list().map((file) =>
-      JSON.parse(
-        fs.readFileSync(path.join(this.pokemonsPath, file, '.json'), 'utf8'),
-      ),
+    return this.list().then((files) =>
+      Promise.all(files.map((file) => this.findOne(file))),
     );
   }
 
-  list(): string[] {
-    if (!fs.existsSync(this.pokemonsPath)) {
+  list(): Promise<string[]> {
+    if (!fileExists(this.pokemonsPath)) {
       throw new HttpException('Pokemons not retrieved.', HttpStatus.NOT_FOUND);
     }
 
     return fs
-      .readdirSync(this.pokemonsPath)
-      .filter((file) => file.endsWith('.json'))
-      .map((file) => file.replace('.json', ''));
+      .readdir(this.pokemonsPath)
+      .then((files) => files.filter((file) => file.endsWith('.json')))
+      .then((files) => files.map((file) => file.replace('.json', '')));
   }
 
-  findOne(name: string): Pokemon {
+  findOne(name: string): Promise<Pokemon> {
     const filePath = path.join(this.pokemonsPath, `${name}.json`);
-    if (!fs.existsSync(filePath)) {
+    if (!fileExists(filePath)) {
       throw new HttpException(
         'This pokemon does not exist',
         HttpStatus.NOT_FOUND,
       );
     }
 
-    return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    return fs.readFile(filePath, 'utf8').then((c) => JSON.parse(c) as Pokemon);
   }
 
-  frontSprite(name: string): Observable<Buffer> {
-    return this.http
-      .get(this.findOne(name).frontSprite, { responseType: 'arraybuffer' })
-      .pipe(map((res) => Buffer.from(res.data)));
+  frontSprite(name: string): Promise<Observable<Buffer>> {
+    return this.findOne(name).then((pokemon) =>
+      this.http
+        .get(pokemon.frontSprite, { responseType: 'arraybuffer' })
+        .pipe(map((res) => Buffer.from(res.data))),
+    );
   }
 
-  backSprite(name: string): Observable<Buffer> {
-    return this.http
-      .get(this.findOne(name).backSprite, { responseType: 'arraybuffer' })
-      .pipe(map((res) => Buffer.from(res.data)));
+  backSprite(name: string): Promise<Observable<Buffer>> {
+    return this.findOne(name).then((pokemon) =>
+      this.http
+        .get(pokemon.backSprite, { responseType: 'arraybuffer' })
+        .pipe(map((res) => Buffer.from(res.data))),
+    );
   }
 }
